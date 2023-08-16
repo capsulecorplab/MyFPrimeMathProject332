8f7344c064de44c649f7d1a5b4579686d38976f1
Sean Marquez <capsulecorplab@gmail.com>
2023-08-16 23:52:11 +0000

    implement parameterUpdated function
    as per https://fprime-community.github.io/fprime-tutorial-math-component/docs/creating-components-4.html#editing-the-throttle-command-handler

--- Components/MathReceiver/MathReceiver.cpp
+++ Components/MathReceiver/MathReceiver.cpp
@@ -110,4 +110,24 @@ namespace MathModule {
       this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
   }
 
+  void MathReceiver ::
+    parameterUpdated(FwPrmIdType id)
+  {
+      switch (id) {
+          case PARAMID_FACTOR: {
+              Fw::ParamValid valid;
+              F32 val = this->paramGet_FACTOR(valid);
+              FW_ASSERT(
+                  valid.e == Fw::ParamValid::VALID || valid.e == Fw::ParamValid::DEFAULT,
+                  valid.e
+              );
+              this->log_ACTIVITY_HI_FACTOR_UPDATED(val);
+              break;
+          }
+          default:
+              FW_ASSERT(0, id);
+              break;
+      }
+  }
+
 } // end namespace MathModule
